2758c6dbcf97342b71161056ede883fbb1e0f1b6
Hariprasad <haridx.poojary@gmail.com>
2021-01-25 20:50:03 +0530

    Change in Hero pattern,work gallery & condition for debug value in dev & prod env

--- hari_portfolio/settings.py
+++ hari_portfolio/settings.py
@@ -32,10 +32,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 if is_prod:
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    DEBUG = False
 else:
     SECRET_KEY = os.getenv("SECRET_KEY")
+    DEBUG = True
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ["127.0.0.1", "hariprasad.herokuapp.com"]
 
